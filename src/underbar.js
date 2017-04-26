1a30193ae252978a41996ee77da7a6b2e5560f2e
Brendan Burns <brendanmioburns@gmail.com>
2017-04-26 03:48:58 -0700

    wrapping up reduce

--- src/underbar.js
+++ src/underbar.js
@@ -7,7 +7,7 @@
   // seem very useful, but remember it--if a function needs to provide an
   // iterator when the user does not pass one in, this will be handy.
   _.identity = function(val) {
-    return val; //////???????????????????
+    return val;
   };
 
   /**
@@ -177,6 +177,14 @@
   //   }); // should be 5, regardless of the iterator function passed in
   //          No accumulator is given so the first element is used.
   _.reduce = function(collection, iterator, accumulator) {
+    _.each(collection, function(item) {
+      if (accumulator === undefined) {
+        accumulator = item;
+      } else {
+        accumulator = iterator(accumulator, item);
+      }
+    })
+    return accumulator;
   };
 
   // Determine if the array or object contains a given value (using `===`).
